0872408c48d79b755db68d65c12abbe04cf8f5bc
Abhishek Kumar <abhishek.kumar2@outlook.com>
2020-08-28 20:51:48 +0800

    postgresql connectivity

--- course-api-data/src/main/java/io/knowabhi/course/CourseController.java
+++ course-api-data/src/main/java/io/knowabhi/course/CourseController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class CourseController {
 	@Autowired
 	private CourseService courseService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("topics/{topicId}/courses")
 	public List<Course> getAllCourses(@PathVariable String topicId) {
 		return courseService.getAllCourses(topicId);
--- course-api-data/src/main/java/io/knowabhi/lesson/LessonController.java
+++ course-api-data/src/main/java/io/knowabhi/lesson/LessonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class LessonController {
 	@Autowired
 	private LessonService lessonService;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("topics/{topicId}/courses/{courseId}/lessons")
 	public List<Lesson> getAllLessons(@PathVariable String courseId) {
 		return lessonService.getAllLessons(courseId);
